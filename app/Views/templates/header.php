1e4412f5ac186c2298395c170c597e3b46421a07
andreasmaulana <154958133+andreasmaulana@users.noreply.github.com>
2025-07-17 02:26:38 +0700

    menambahkan file templates berisi footer dan header

---
+++ app/Views/templates/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Green City</title>
+    <link rel="stylesheet" href="<?= base_url('assets/style.css') ?>">
+</head>
+<body>
