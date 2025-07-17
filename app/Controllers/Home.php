524eb832a27802511956f444ef7bb3e7bcc84abf
dwihikmal <tsaqifulilmi56@gmail.com>
2025-07-17 23:47:19 +0700

    Merapikan Files

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,45 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
-        return view('welcome_message');
+        return view('templates/header')
+             . view('home')
+             . view('templates/footer');
+    }
+
+    public function tentang()
+    {
+        return view('templates/header')
+             . view('tentang')
+             . view('templates/footer');
+    }
+
+    public function tipeRumah()
+    {
+        return view('templates/header')
+             . view('tipe-rumah')
+             . view('templates/footer');
+    }
+
+    public function galeri()
+    {
+        return view('templates/header')
+             . view('galeri')
+             . view('templates/footer');
+    }
+
+    public function lokasi()
+    {
+        return view('templates/header')
+             . view('lokasi')
+             . view('templates/footer');
+    }
+
+    public function kontak()
+    {
+        return view('templates/header')
+             . view('kontak')
+             . view('templates/footer');
     }
 }
